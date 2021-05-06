64dca778f41f9e715c36a7b8291722c7e7c85178
Tomek Regulski <75456264+tomekregulski@users.noreply.github.com>
2021-05-06 09:18:57 -0400

    Add login mock-up. Adjust nav and property search partials

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -24,4 +24,12 @@ router.get("/edit-listing", async (req, res) => {
   }
 });
 
+router.get("/login", async (req, res) => {
+  try {
+    res.render("login");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
