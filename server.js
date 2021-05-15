c9293af9510abd015ea649a613ab3a2fe55f5540
Tomek Regulski <75456264+tomekregulski@users.noreply.github.com>
2021-05-15 14:44:04 -0400

    Add secret to .env

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const path = require('path');
-const session = require('express-session');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-const exphbs = require('express-handlebars');
-const sequelize = require('./config/connection');
-const helpers = require('./utils/helpers');
-const bodyParser = require('body-parser');
+const express = require("express");
+const path = require("path");
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const exphbs = require("express-handlebars");
+const sequelize = require("./config/connection");
+const helpers = require("./utils/helpers");
+const bodyParser = require("body-parser");
 
 const hbs = exphbs.create({ helpers });
 
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 8080;
 
 // Create a new sequelize store using the express-session package
 const sess = {
-  secret: 'SECRET', // key to sign the cookie
+  secret: process.env.SECRET, // key to sign the cookie
   cookie: {},
   resave: false,
   saveUninitialized: true,
@@ -25,15 +25,15 @@ const sess = {
 };
 
 app.use(session(sess));
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 // app.use(express.json());
 // app.use(express.urlencoded({ extended: false }));
-app.use(express.json({ limit: '25mb' }));
-app.use(express.urlencoded({ limit: '25mb', extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(require('./controllers'));
+app.use(express.json({ limit: "25mb" }));
+app.use(express.urlencoded({ limit: "25mb", extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(require("./controllers"));
 
 // app.use(express.limit('50M'));
 // app.use(bodyParser.json({ limit: '50mb' }));
@@ -47,5 +47,5 @@ app.use(require('./controllers'));
 
 // Starts the server to begin listening
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log("Now listening"));
 });
