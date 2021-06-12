448559c0a7b9f9907126b54e12724657b10f8e5c
yondav <yonidavid.abr@gmail.com>
2021-06-11 21:13:50 -0400

    updated unit search form neighborhoods

--- public/js/property-search.js
+++ public/js/property-search.js
@@ -179,23 +179,31 @@ const toggleSearchBtn = () => {
 const uncheckAll = () => document.getElementsByName('searchElement').forEach((el) => (el.checked = false));
 
 // expand neighborhoods in accordian filter
-document.querySelector('.brooklyn').addEventListener('click', () => {
-  document.querySelectorAll('.brooklyn-neighborhoods').forEach((neighborhood) => {
-    neighborhood.classList.toggle('hide');
+document.querySelectorAll('.brooklyn').forEach((neighborhood) => {
+  neighborhood.addEventListener('click', () => {
+    document.querySelectorAll('.brooklyn-neighborhoods').forEach((neighborhood) => {
+      neighborhood.classList.toggle('hide');
+    });
   });
 });
-document.querySelector('.bronx').addEventListener('click', () => {
-  document.querySelectorAll('.bronx-neighborhoods').forEach((neighborhood) => {
-    neighborhood.classList.toggle('hide');
+document.querySelectorAll('.bronx').forEach((neighborhood) => {
+  neighborhood.addEventListener('click', () => {
+    document.querySelectorAll('.bronx-neighborhoods').forEach((neighborhood) => {
+      neighborhood.classList.toggle('hide');
+    });
   });
 });
-document.querySelector('.manhattan').addEventListener('click', () => {
-  document.querySelectorAll('.manhattan-neighborhoods').forEach((neighborhood) => {
-    neighborhood.classList.toggle('hide');
+document.querySelectorAll('.manhattan').forEach((neighborhood) => {
+  neighborhood.addEventListener('click', () => {
+    document.querySelectorAll('.manhattan-neighborhoods').forEach((neighborhood) => {
+      neighborhood.classList.toggle('hide');
+    });
   });
 });
-document.querySelector('.queens').addEventListener('click', () => {
-  document.querySelectorAll('.queens-neighborhoods').forEach((neighborhood) => {
-    neighborhood.classList.toggle('hide');
+document.querySelectorAll('.queens').forEach((neighborhood) => {
+  neighborhood.addEventListener('click', () => {
+    document.querySelectorAll('.queens-neighborhoods').forEach((neighborhood) => {
+      neighborhood.classList.toggle('hide');
+    });
   });
 });
